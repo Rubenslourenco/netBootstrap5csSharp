380bda6b72535c6fe39c19b18adf10b225175a71
Rubenslourenco <alvesrubens926@gmail.com>
2025-03-20 15:10:34 -0300

    Inicializando projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace netBootstrap5csSharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
